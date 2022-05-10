1f05f4affa41b18c1c1bd189daf85c0328d3d95e
IvanPKostadinov <ikostadinov@melontech.com>
2022-05-10 12:38:37 +0300

    added logic to extract the event Id from the url for the Event Detail page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,21 @@
+import { useRouter } from 'next/router';
+
+import { getEventById } from '../../dummy-data';
+
 function EventDetailPage() {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
   return (
     <div>
-      <h1>Event Detail Page</h1>
+      <h1>{event.title}</h1>
     </div>
   );
 }
