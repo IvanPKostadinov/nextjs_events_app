7e955ec926bfafcbbdea7fcaa1c6aab4e89b6f4a
IvanPKostadinov <ikostadinov@melontech.com>
2022-05-10 15:36:53 +0300

    in [...slug].js (catch-all page) extracted the data from the URL and set some if conditions for checks

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,4 +1,43 @@
+import router, { useRouter } from 'next/router';
+
+import { getFilteredEvents } from '../../dummy-data';
+import EventList from '../../components/events/event-list';
+
 function FilteredEventsPage() {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className='center'>Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p className='center'>Invalid filter</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p className='center'>No Events Found.</p>
+  }
+
   return (
     <div>
       <h1>Filtered Events Page</h1>
