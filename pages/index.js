e71ab6858bc74717ae36461fbebbc6d65bcaee7f
IvanPKostadinov <ikostadinov@melontech.com>
2022-05-11 22:55:27 +0300

    added Static Site Generation (SSG) on the Home Page for fetching the featured events

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
 import EventList from '../components/events/event-list';
-import { getFeaturedEvents } from '../dummy-data';
+import { getFeaturedEvents } from '../helpers/api-util';
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
-
+function HomePage(props) {
   return (
     <div>
-      <EventList items={featuredEvents} />
+      <EventList items={props.events} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+  };
 }
 
 export default HomePage;
