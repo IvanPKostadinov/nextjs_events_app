42440981de492aaa9208b36730b570e95e18906c
IvanPKostadinov <ikostadinov@melontech.com>
2022-05-12 14:17:28 +0300

    added <Head> data to _app.js that will be merged or overwritten by other components down the tree

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,11 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
+        {/* If some of these elements are repeated down the tree, they will be overwritten.
+            If not - they will be merged.
+            If we want repeating elements -> we add key="some-key" */}
+        <title>Next Events</title>
+        <meta name='description' content='Next Events' />
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
       <Component {...pageProps} />
