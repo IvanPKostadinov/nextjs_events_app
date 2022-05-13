b4d3f87ee6e050a60d84f4fdb78c6f55701b3919
IvanPKostadinov <ikostadinov@melontech.com>
2022-05-13 16:15:37 +0300

    fetched the Comments from the Comments API Route and listed them in CommentList Component

--- components/input/comment-list.js
+++ components/input/comment-list.js
@@ -1,21 +1,23 @@
 import classes from './comment-list.module.css';
 
-function CommentList() {
+function CommentList(props) {
+  const { items } = props;
+
+  if (items.length === 0) {
+    return <p>No comments yet.</p>;
+  }
+
   return (
     <ul className={classes.comments}>
       {/* Render list of comments - fetched from API */}
-      <li>
-        <p>My comment is amazing!</p>
-        <div>
-          By <address>Maximilian</address>
-        </div>
-      </li>
-      <li>
-        <p>My comment is amazing!</p>
-        <div>
-          By <address>Maximilian</address>
-        </div>
-      </li>
+      {items.map((comment) => (
+        <li key={comment.id}>
+          <p>{comment.text}</p>
+          <div>
+            By <address>{comment.name}</address>
+          </div>
+        </li>
+      ))}
     </ul>
   );
 }
--- components/input/comments.js
+++ components/input/comments.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import CommentList from './comment-list';
 
 import classes from './comments.module.css';
@@ -8,6 +8,17 @@ function Comments(props) {
   const { eventId } = props;
 
   const [showComments, setShowComments] = useState(false);
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    if (showComments) {
+      fetch(`/api/comments/${eventId}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setComments(data.comments);
+        });
+    }
+  }, [showComments]);
 
   function toggleCommentsHandler() {
     setShowComments((prevStatus) => !prevStatus);
@@ -33,7 +44,7 @@ function Comments(props) {
         {showComments ? 'Hide' : 'Show'} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList />}
+      {showComments && <CommentList items={comments}/>}
     </section>
   );
 }
