0d26785069acc033378ecd5610dbec7925267b94
IvanPKostadinov <ikostadinov@melontech.com>
2022-05-15 17:22:42 +0300

    added NotificationContext to the Layout Component and used it there to show/hide the <Notification /> Component with the appropriate data

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,14 +1,25 @@
-import { Fragment } from 'react';
+import { Fragment, useContext } from 'react';
 
 import MainHeader from './main-header';
 import Notification from '../ui/notification';
+import NotificationContext from '../../store/notification-context';
 
 function Layout(props) {
+  const notificationCtx = useContext(NotificationContext);
+
+  const activeNotification = notificationCtx.notification;
+
   return (
     <Fragment>
       <MainHeader />
       <main>{props.children}</main>
-      <Notification title='Test' message='This is a test' status='pending' />
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
     </Fragment>
   );
 }
