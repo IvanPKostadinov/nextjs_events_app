2ac9485c5152f9af7a45a9691aeb27c5496e9c33
IvanPKostadinov <ikostadinov@melontech.com>
2022-05-15 17:06:53 +0300

    provided value for the context in NotificationContextProvided, which will provide it app-wide

--- store/notification-context.js
+++ store/notification-context.js
@@ -1,14 +1,30 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 const NotificationContext = createContext({
   notification: null, // { title, message, status}
-  showNotification: function () {},
+  showNotification: function (notificationData) {},
   hideNotification: function () {},
 });
 
 export function NotificationContextProvider(props) {
+  const [activeNotification, setActiveNotification] = useState();
+
+  function showNotificationHandler(notificationData) {
+    setActiveNotification(notificationData);
+  }
+
+  function hideNotificationHandler() {
+    setActiveNotification(null);
+  }
+
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  };
+
   return (
-    <NotificationContext.Provider>
+    <NotificationContext.Provider value={context}>
       {props.children}
     </NotificationContext.Provider>
   );
