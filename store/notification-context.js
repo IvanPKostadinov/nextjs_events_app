0ced5e1e86c621ebafaf212490fb0255e490be95
IvanPKostadinov <ikostadinov@melontech.com>
2022-05-16 14:41:29 +0300

    added a timer to hide the Notification banner after 3 seconds

--- store/notification-context.js
+++ store/notification-context.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 const NotificationContext = createContext({
   notification: null, // { title, message, status}
@@ -8,6 +8,23 @@ const NotificationContext = createContext({
 
 export function NotificationContextProvider(props) {
   const [activeNotification, setActiveNotification] = useState();
+
+  useEffect(() => {
+    if (
+      activeNotification &&
+      (activeNotification.status === 'success' ||
+        activeNotification.status === 'error')
+    ) {
+      const timer = setTimeout(() => {
+        setActiveNotification(null);
+      }, 3000);
+
+      // Here we clear the timer, in case useEffect is triggered before the first timer passes:
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [activeNotification]);
 
   function showNotificationHandler(notificationData) {
     setActiveNotification(notificationData);
